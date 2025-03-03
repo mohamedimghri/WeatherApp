0daafdde29a8e0b0a9cd917046e49b7cf3e9bd46
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-03 00:18:23 +0000

    weather_page modified

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
-class WeatherPage extends StatelessWidget {
+class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
 
   @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+
+    );
   }
 }
