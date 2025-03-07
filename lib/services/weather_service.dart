e6afa4b542075dd273477dcf1b8b6b559bf8e3a3
Mohamed Imghri <155360838+mohamedimghri@users.noreply.github.com>
2025-03-07 16:34:25 +0000

    weather API modified

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -5,7 +5,7 @@ import 'package:weather_app/models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+  static const BASE_URL = 'http://api.openweathermap.org/data/2.5';
   final String apiKey;
 
   WeatherService(this.apiKey);
